3d68ae3eaeff5f5e7cafc392ea4c4954b6d424d7
FabioCoutinho <fabiofarias549@gmail.com>
2025-01-13 14:53:39 -0300

    Update scrips.js

--- scrips.js
+++ scrips.js
@@ -11,190 +11,190 @@ let arrTarefa = [];
 let oldvalvue = "";
 
 const cratTarefa = (text) => {
-    const divTarefa = document.createElement("div");
-    divTarefa.classList.add("tarefa");
+  const divTarefa = document.createElement("div");
+  divTarefa.classList.add("tarefa");
 
-    boxTarefa.appendChild(divTarefa);
+  boxTarefa.appendChild(divTarefa);
 
-    setTimeout(() => {
-        divTarefa.classList.add("visible");
-    }, 100);
+  setTimeout(() => {
+    divTarefa.classList.add("visible");
+  }, 100);
 
-    const textoTarefa = document.createElement("h3");
-    textoTarefa.innerText = text;
-    divTarefa.appendChild(textoTarefa);
+  const textoTarefa = document.createElement("h3");
+  textoTarefa.innerText = text;
+  divTarefa.appendChild(textoTarefa);
 
-    const checkBtn = document.createElement("button");
-    checkBtn.classList.add("check");
-    checkBtn.innerHTML = '<span class="material-symbols-outlined"> check </span>';
-    divTarefa.appendChild(checkBtn);
+  const checkBtn = document.createElement("button");
+  checkBtn.classList.add("check");
+  checkBtn.innerHTML = '<span class="material-symbols-outlined"> check </span>';
+  divTarefa.appendChild(checkBtn);
 
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("edit");
-    editBtn.innerHTML = '<span class="material-symbols-outlined"> edit </span>';
-    divTarefa.appendChild(editBtn);
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit");
+  editBtn.innerHTML = '<span class="material-symbols-outlined"> edit </span>';
+  divTarefa.appendChild(editBtn);
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete");
-    deleteBtn.innerHTML =
-        '<span class="material-symbols-outlined"> delete </span>';
-    divTarefa.appendChild(deleteBtn);
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete");
+  deleteBtn.innerHTML =
+    '<span class="material-symbols-outlined"> delete </span>';
+  divTarefa.appendChild(deleteBtn);
 };
 
 const animarMain = () => {
-    main.style.transform = "scale(1)";
+  main.style.transform = "scale(1)";
 
-    setTimeout(() => {
-        main.style.transform = "scale(0.98)";
-    }, 500);
+  setTimeout(() => {
+    main.style.transform = "scale(0.98)";
+  }, 500);
 };
 
-const remove = (element) => {
-    setTimeout(() => {
-        element.classList.add("remove");
-    }, 100);
+const removeDiv = (element) => {
+  setTimeout(() => {
+    element.classList.add("remove");
+  }, 100);
 };
 
 const saveStorage = (data) => {
-    if (localStorage.myarr) {
-        arrTarefa = JSON.parse(localStorage.getItem("myarr"));
-    }
+  if (localStorage.myarr) {
+    arrTarefa = JSON.parse(localStorage.getItem("myarr"));
+  }
 
-    arrTarefa.push(data);
-    localStorage.myarr = JSON.stringify(arrTarefa);
-    return arrTarefa;
+  arrTarefa.push(data);
+  localStorage.myarr = JSON.stringify(arrTarefa);
+  return arrTarefa;
 };
 
 function loadinPage() {
-    if (localStorage.myarr) {
-        arrTarefa = JSON.parse(localStorage.getItem("myarr"));
+  if (localStorage.myarr) {
+    arrTarefa = JSON.parse(localStorage.getItem("myarr"));
+  }
+
+  console.log(arrTarefa);
+
+  arrTarefa.forEach((item) => {
+    cratTarefa(item.tarefa);
+    if (item.checked) {
+      const div = boxTarefa.lastElementChild;
+      div.classList.add("checked");
     }
+  });
 
-    console.log(arrTarefa);
+  setInterval(() => {
+    main.style.opacity = "1";
+  }, 500);
 
-    arrTarefa.forEach((item) => {
-        cratTarefa(item.tarefa);
-        if (item.checked) {
-            const div = boxTarefa.lastElementChild;
-            div.classList.add("checked");
-        }
-    });
-
-    setInterval(() => {
-        main.style.opacity = "1";
-    }, 500);
-
-    input.focus();
+  input.focus();
 }
 
 const mostrarEnaoMostrar = () => {
-    form.classList.toggle("hidden");
-    editForm.classList.toggle("hidden");
-    boxTarefa.classList.toggle("hidden");
+  form.classList.toggle("hidden");
+  editForm.classList.toggle("hidden");
+  boxTarefa.classList.toggle("hidden");
 };
 
 const editTarefas = (text) => {
-    const div = boxTarefa.querySelector(".tarefa");
-    const h3 = div.querySelector("h3");
+  const div = boxTarefa.querySelector(".tarefa");
+  const h3 = div.querySelector("h3");
 
-    h3.innerText = text;
+  h3.innerText = text;
 
-    arrTarefa = arrTarefa.map((item) => {
-        if (item.tarefa === oldvalvue) {
-            item.tarefa = text;
-            localStorage.myarr = JSON.stringify(arrTarefa);
-        }
-        return item;
-    });
+  arrTarefa = arrTarefa.map((item) => {
+    if (item.tarefa === oldvalvue) {
+      item.tarefa = text;
+      localStorage.myarr = JSON.stringify(arrTarefa);
+    }
+    return item;
+  });
 };
 
 //enventos
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
 
-    const { tarefa } = data;
-    animarMain();
-    cratTarefa(tarefa);
-    saveStorage(data);
+  const { tarefa } = data;
+  animarMain();
+  cratTarefa(tarefa);
+  saveStorage(data);
 
-    input.value = "";
-    input.focus();
+  input.value = "";
+  input.focus();
 });
 
 document.addEventListener("click", (e) => {
-    const element = e.target;
-    const parent = element.closest("div");
+  const element = e.target;
+  const parent = element.closest("div");
 
-    let title;
+  let title;
 
-    if (parent && parent.querySelector("h3")) {
-        title = parent.querySelector("h3").innerText;
-    }
+  if (parent && parent.querySelector("h3")) {
+    title = parent.querySelector("h3").innerText;
+  }
 
-    if (element.classList.contains("check")) {
-        parent.classList.toggle("checked");
-        if (parent.classList.contains("checked")) {
-            arrTarefa = arrTarefa.map((item) => {
-                if (item.tarefa === title) {
-                    item.checked = true;
-                    localStorage.myarr = JSON.stringify(arrTarefa);
-                }
-                return item;
-            });
-        } else {
-            arrTarefa = arrTarefa.map((item) => {
-                if (item.tarefa === title) {
-                    item.checked = false;
-                    localStorage.myarr = JSON.stringify(arrTarefa);
-                }
-                return item;
-            });
+  if (element.classList.contains("check")) {
+    parent.classList.toggle("checked");
+    if (parent.classList.contains("checked")) {
+      arrTarefa = arrTarefa.map((item) => {
+        if (item.tarefa === title) {
+          item.checked = true;
+          localStorage.myarr = JSON.stringify(arrTarefa);
         }
+        return item;
+      });
+    } else {
+      arrTarefa = arrTarefa.map((item) => {
+        if (item.tarefa === title) {
+          item.checked = false;
+          localStorage.myarr = JSON.stringify(arrTarefa);
+        }
+        return item;
+      });
     }
+  }
 
-    if (element.classList.contains("edit")) {
-        mostrarEnaoMostrar();
-        animarMain();
+  if (element.classList.contains("edit")) {
+    mostrarEnaoMostrar();
+    animarMain();
 
-        editInput.value = title;
-        oldvalvue = title;
-        console.log(oldvalvue);
+    editInput.value = title;
+    oldvalvue = title;
+    console.log(oldvalvue);
 
-        editInput.focus();
-    }
+    editInput.focus();
+  }
 
-    if (element.classList.contains("delete")) {
-        const div = element.parentNode;
+  if (element.classList.contains("delete")) {
+    const div = element.parentNode;
 
-        remove(div);
+    removeDiv(div);
 
-        setTimeout(() => {
-            parent.remove();
-            animarMain();
-            arrTarefa = arrTarefa.filter((item) => item.tarefa !== title);
-            localStorage.myarr = JSON.stringify(arrTarefa);
-        }, 500);
-    }
+    setTimeout(() => {
+      parent.remove();
+      animarMain();
+      arrTarefa = arrTarefa.filter((item) => item.tarefa.trim() !== title);
+      localStorage.myarr = JSON.stringify(arrTarefa);
+    }, 500);
+  }
 });
 
 cancalBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    mostrarEnaoMostrar();
-    animarMain();
+  mostrarEnaoMostrar();
+  animarMain();
 });
 
 editForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const editInputValue = editInput.value;
+  const editInputValue = editInput.value;
 
-    if (editInputValue) {
-        editTarefas(editInputValue);
-    }
+  if (editInputValue) {
+    editTarefas(editInputValue);
+  }
 
-    mostrarEnaoMostrar();
-    animarMain();
+  mostrarEnaoMostrar();
+  animarMain();
 });
